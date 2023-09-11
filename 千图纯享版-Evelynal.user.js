4f6593c1229888664551c4a942d532b2cf9a0d36
Evelynl <Evelynalll@outlook.com>
2023-09-11 15:19:35 +0800

    Add files via upload

---
+++ 千图纯享版-Evelynal.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         千图纯享版-Evelynal
+// @namespace    http://www.evelynal.top/Navigation/
+// @version      0.1
+// @description  删除多余功能按钮,送你一个干净的千图
+// @author       Evelynal
+// @match        https://www.58pic.com/
+// @match       https://www.58pic.com/tupian/*
+// @icon            https://icon.qiantucdn.com/images/user/user-default.png
+// @grant         none
+// @require     http://libs.baidu.com/jquery/2.0.0/jquery.min.js
+// @license      AGPL-3.0 license
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // Your code here...
+    //删除顶栏
+    $(".head-box").remove();
+    $(".banner-container").remove();
+    //    $(".banner-pagination").remove();
+    //    $(".banner-tip").remove();
+    $(".floor-item").remove();
+    $(".new-copyright-container").remove();
+    $(".designer-entry-container").remove();
+    $(".seo-link-container").remove();
+    $(".hot-keyword-container").remove();
+    $(".qtd-search-containerB").remove();
+    $(".search-polymerization-list-wrap").remove();
+
+    //修改视频格子的margin-top
+    const feedCards = $('.search-category-container');
+    feedCards.css('margin-top', '0');
+    feedCards.css('margin-bottom', '600px');
+
+    // 创建img
+    var img = $('<img>');
+    img.attr('src', 'https://icon.qiantucdn.com/static/images/logo/p-logo-web1.png');
+
+    // 设置img的css
+    img.css({
+        width: '100px',
+        height: 'auto',
+        'margin-top': '20vh',
+        'margin-left':'45%'
+    });
+
+    // 选择id为qt-app的div
+    var $div = $('.qt-app-box');
+
+    // 在div之前插入img
+    $div.before(img);
+
+})();
